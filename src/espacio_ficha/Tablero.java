7aa935dd5588a98adf9a79597c314f608900d31e
Juan Felipe Fontecha <USUARIO@USUARIO-PC>
2016-11-25 17:28:27 -0500

    Version 2.3 completo la vesrion 1.0 del mapa de parques(2 carceles, 2 torres y un camino de 9 casillas de largo) completo movimiento normal y por las torres(para este mapa y sin pruebas exahustivas) *falta la opcion de comer, el uso de las carceles, los dados y la forma como hacer que las fichas entren en las torres en un primer lugar

--- src/espacio_ficha/Tablero.java
+++ src/espacio_ficha/Tablero.java
@@ -5,8 +5,8 @@
  */
 package espacio_ficha;
 
+import Excepciones.ExcepcionA;
 import java.util.*;
-
 /**
  *
  * @author USUARIO
@@ -73,12 +73,29 @@ public class Tablero {
         }
         return salir;
     }
-    public void determinarSiPasaPorSeguroTorre(int movimiento, Ficha f){
-        
+    //solo se usa en las casillas "externas"
+    public int PasaPorSeguroTorre(int movimiento, Ficha f){
+        int mov=movimiento;
+        for (int i = f.getCasilla().getCamino()+1; i < movimiento; i++) {
+            System.out.println("i "+i);
+            //se determina asi porque el "numero" de la casilla de torre es igual al "id" de la ficha que lo puede subir
+            if (this.casilla1[1][i].getTipo().getNumero()==f.getTipo().getId()) {
+                f.getCasilla().getCantidad().remove(f);
+                this.casilla1[1][i].getCantidad().add(f);
+                f.setCasilla(this.casilla1[1][i]);
+                mov=movimiento-i+1;
+                i=movimiento;
+            } else {
+            }
+        }
+        return mov;
     }
-    public void mover(int movimiento, Ficha f){
+    
+    /*throw en caso de que la ficha se pase de la torre con el movimiento y asi se evita que el programa 
+    muera se quito un if que cumplia esta funcion ya que la excepcion cumple mejor esta funcion*/
+    public void mover(int movimiento, Ficha f) throws ExcepcionA {
         int posicion = this.determinar_posicion(this.casilla1[1], f);
-        if (this.casilla1[2][f.getCasilla().getCamino()] == null) {
+        if (f.getCasilla().getCamino() < 100 && f.getCasilla().getTipo().getNumero() == 0) {
             //movimiento normal
             movimiento += posicion;
             this.casilla1[1][posicion].getCantidad().remove(f);
@@ -94,38 +111,49 @@ public class Tablero {
             //movimiento en torres
             switch (f.getTipo().getId()) {
                 case 1:
-                    //revisar el numero por el que se mide el movimiento
-                    if (movimiento > (5 - (f.getCasilla().getCamino() - 100))) {
-                        //no mover
-                    } else // en caso de que este en el seguro de la torre 
-                        if (f.getCasilla().getCamino() < 100) {
+                    /*revisar el numero por el que se mide el movimiento
+                    en caso de que este en el seguro de la torre*/ 
+                    if (f.getCasilla().getCamino() < 100) {
                         this.casilla1[1][posicion].getCantidad().remove(f);
-                        this.casilla1[movimiento][5].getCantidad().add(f);
-                        f.setCasilla(this.casilla1[movimiento][5]);
-
+                        //por el lugar donde empieza
+                        this.casilla1[movimiento + 1][4].getCantidad().add(f);
+                        f.setCasilla(this.casilla1[movimiento + 1][4]);
                     } else {
-                        this.casilla1[1][posicion - 100].getCantidad().remove(f);
-                        this.casilla1[movimiento][5].getCantidad().add(f);
-                        f.setCasilla(this.casilla1[movimiento][5]);
+                        /*99 sale de la diferencia entre el "camino" especifico del jugador y el hecho de que
+                        tiene que estar mas abajo que la fila 1 (fial 0, fila 1, ...)
+                        el cuatro sale de la columna en la que esta la torre del jugador*/
+                        this.casilla1[f.getCasilla().getCamino() - 99][4].getCantidad().remove(f);
+                        this.casilla1[movimiento + f.getCasilla().getCamino() - 99][4].getCantidad().add(f);
+                        f.setCasilla(this.casilla1[movimiento + f.getCasilla().getCamino() - 99][4]);
                     }
                     break;
                 default:
                     break;
             }
         }
-
     }
-    /*
+    /* no funciona porque el metodo ".get(i)   int i" obtendra un objeto "object" y no una ficha
+    revisar como obtener un objeto en especifico en un arraylist si se quiere usar el metodo de abajo para comer
     public void comer(Casilla c, Ficha f){
+        //solo se usara en las casillas "externas"
         for (int i = 0; i < c.getCantidad().size(); i++) {
             Ficha comprobante=(Ficha) c.getCantidad().get(i);
-            if (comprobante.getTipo()!=f.getTipo()) {
-            casilla1[0][0].getCantidad().add(c.getCantidad().get(i));
-            c.getCantidad().remove(i);
+            //un caso por cada jugador diferente
+            if (comprobante.getTipo().getId()!=f.getTipo().getId()) {
+                switch(comprobante.getTipo().getId()){
+                    case 1:
+                        //se anda la ficha enemiga a su carcel
+                        casilla1[0][2].getCantidad().add(c.getCantidad().get(i));
+                        c.getCantidad().remove(i);
+                        break;
+                        //ultimo jugador
+                    default:
+                        casilla1[0][6].getCantidad().add(c.getCantidad().get(i));
+                        c.getCantidad().remove(i);
+                        break;
+                }
             }  
         }
-         
     }
 */
-     
 }
