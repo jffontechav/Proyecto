a93c7c83ff9d766b2d3148e7e0c54077c6206937
Juan Felipe Fontecha <USUARIO@USUARIO-PC>
2016-11-25 17:28:16 -0500

    Version 2.3 completo la vesrion 1.0 del mapa de parques(2 carceles, 2 torres y un camino de 9 casillas de largo) completo movimiento normal y por las torres(para este mapa y sin pruebas exahustivas) *falta la opcion de comer, el uso de las carceles, los dados y la forma como hacer que las fichas entren en las torres en un primer lugar

--- src/espacio_ficha/Main.java
+++ src/espacio_ficha/Main.java
@@ -79,11 +79,13 @@ public static void main(String[] args) {
         Casilla c2 = new Casilla(null,tc1);
         Casilla c3 = new Casilla(null,tc1);
         Casilla c4 = new Casilla(null,tc1);
-        Casilla c5 = new Casilla(null,tc1);
+        //casilla de torre de entrada jagador 1
+        Casilla c5 = new Casilla(null,tc2_1);
         Casilla c6 = new Casilla(null,tc1);
         Casilla c7 = new Casilla(null,tc1);
         Casilla c8 = new Casilla(null,tc1);
-        Casilla c9 = new Casilla(null,tc1);
+        //casilla de torre de entrada jagador 2
+        Casilla c9 = new Casilla(null,tc2_2);
         Casilla[] c_1 = {c1,c2,c3,c4,c5,c6,c7,c8,c9}; 
         c_1[0].setCantidad(list1);
         c_1[1].setCantidad(list2);
@@ -115,40 +117,26 @@ public static void main(String[] args) {
         ct1_1.setCamino(101);
         Casilla ct1_2 = new Casilla(null,tc1);
         ct1_2.setCantidad(listtorre1_2);
-        ct1_1.setCamino(102);
+        ct1_2.setCamino(102);
         Casilla ct1_3 = new Casilla(null,tc1);
         ct1_3.setCantidad(listtorre1_3);
-        ct1_1.setCamino(103);
+        ct1_3.setCamino(103);
         Casilla ct1_4 = new Casilla(null,tc4);
         ct1_4.setCantidad(listtorre1_4);
-        ct1_1.setCamino(104);
+        ct1_4.setCamino(104);
         //torre 2
         Casilla ct2_1 = new Casilla(null,tc1);
         ct2_1.setCantidad(listtorre2_1);
         ct2_1.setCamino(201);
         Casilla ct2_2 = new Casilla(null,tc1);
         ct2_2.setCantidad(listtorre2_2);
-        ct2_1.setCamino(202);
+        ct2_2.setCamino(202);
         Casilla ct2_3 = new Casilla(null,tc1);
         ct2_3.setCantidad(listtorre2_3);
-        ct2_1.setCamino(203);
+        ct2_3.setCamino(203);
         Casilla ct2_4 = new Casilla(null,tc4);
         ct2_4.setCantidad(listtorre2_4);
-        ct2_1.setCamino(204);
-        
-        //aca se crea la situación inicial
-        c_1[1].setFicha(f1);
-        c_1[1].getCantidad().add(f1[0]);
-        c_1[1].getCantidad().add(f1[1]);
-        f1[0].setCasilla(c_1[1]);
-        f1[1].setCasilla(c_1[1]);
-        
-        c_1[2].setFicha(f2);
-        c_1[2].getCantidad().add(f2[0]);
-        c_1[2].getCantidad().add(f2[1]);
-        f2[0].setCasilla(c_1[2]);
-        f2[1].setCasilla(c_1[2]);
-        
+        ct2_4.setCamino(204);
         
         //determina la posicion de la casilla en el arreglo para facilitar varias operaciones
         for (int i = 0; i < c_1.length; i++) {
@@ -168,14 +156,22 @@ public static void main(String[] args) {
         c[4][8]=ct2_3;
         c[5][8]=ct2_4;
         
+        //aca se crea la situación inicial
+        c[1][0].getCantidad().add(f1[0]);
+        c_1[1].getCantidad().add(f1[1]);
+        f1[0].setCasilla(c[1][0]);
+        f1[1].setCasilla(c_1[1]);
         
+        c_1[2].getCantidad().add(f2[0]);
+        c_1[2].getCantidad().add(f2[1]);
+        f2[0].setCasilla(c_1[2]);
+        f2[1].setCasilla(c_1[2]);
         //creacion del tablero
         Tablero tablero = new Tablero(c,f1);
             //el primer turno es el del jugador 1
             int turno=1;
             //variable que determinara el ganador
             int victoria=0;
-            
             while(victoria==0){
                 
                 System.out.println("Turno jugador "+ turno);
@@ -183,17 +179,46 @@ public static void main(String[] args) {
                 System.out.println("Escoja la ficha a mover");
                 int eleccion=teclado.nextInt();
                 int movimiento;
-                //cambiar luego por switch
+                //cambiar luego por switch con tantos casos como jugadores
                 if (turno ==1) {
                     switch(eleccion){
                     case 1:System.out.println("Digite las casillas a mover");
                     movimiento = teclado.nextInt();
-                    tablero.mover(movimiento, f1[0]);
+                    //revisar luego como usar la excepcion personalizada para sacar el mensaje
+                    try{
+                        //si esta en la carcel
+                        if (f1[0].getCasilla().getCamino()<0) {
+                            //implementacion cuando se trabaje con dados
+                        } else
+                        /*no necesita comprobar si pasa por seguro si ya esta en torre o en su seguro
+                        se puede dejar el 100 en la comprobacion de todos los jugadores porque todos los demas caminos de
+                        sus torres tambien son mayores que 100*/
+                        if (f1[0].getCasilla().getTipo().getNumero()==f1[0].getTipo().getId()||f1[0].getCasilla().getCamino()>100) {
+                            tablero.mover(movimiento, f1[0]);
+                        } else {
+                            /*el metodo retorna un entero porque no puede mover la ficha directamente asi que si pone la ficha 
+                            en el seguro de su torre tiene que moverse lo que quede hacia arriba*/
+                            tablero.mover(tablero.PasaPorSeguroTorre(movimiento, f1[0]), f1[0]);
+                        }
+                    }catch (Exception e){
+                        System.out.println("No se puede mover la ficha tanto");
+                    }  
                     System.out.println("Casilla donde termino la ficha: "+(f1[0].getCasilla().getCamino()+1));
                         break;
                     case 2:System.out.println("Digite las casillas a mover");
                     movimiento = teclado.nextInt();
-                    tablero.mover(movimiento, f1[1]);
+                    try{
+                        if (f1[1].getCasilla().getCamino()<0) {
+                            //implementacion cuando se trabaje con dados
+                        } else if (f1[1].getCasilla().getTipo().getNumero()==f1[1].getTipo().getId()
+                                ||f1[1].getCasilla().getCamino()>100) {
+                            tablero.mover(movimiento, f1[1]);
+                        } else {
+                            tablero.mover(tablero.PasaPorSeguroTorre(movimiento, f1[1]), f1[1]);
+                        }
+                    }catch (Exception e){
+                        System.out.println("No se puede mover la ficha tanto");
+                    }
                     System.out.println("Casilla donde termino la ficha: "+(f1[1].getCasilla().getCamino()+1));
                         break;
                     default:System.out.println("Eleccion inaceptable");
@@ -204,12 +229,34 @@ public static void main(String[] args) {
                     switch(eleccion){
                     case 1:System.out.println("Digite las casillas a mover");
                     movimiento = teclado.nextInt();
-                    tablero.mover(movimiento, f2[0]);
+                    try{
+                        if (f2[0].getCasilla().getCamino()<0) {
+                            //implementacion cuando se trabaje con dados
+                        } else if (f2[0].getCasilla().getTipo().getNumero()==f2[0].getTipo().getId()
+                                ||f2[0].getCasilla().getCamino()>100) {
+                            tablero.mover(movimiento, f2[0]);
+                        } else {
+                            tablero.mover(tablero.PasaPorSeguroTorre(movimiento, f2[0]), f2[0]);
+                        }
+                    }catch (Exception e){
+                        System.out.println("No se puede mover la ficha tanto");
+                    }
                         System.out.println("Casilla donde termino la ficha: "+(f2[0].getCasilla().getCamino()+1));
                         break;
                     case 2:System.out.println("Digite las casillas a mover");
                     movimiento = teclado.nextInt();
-                    tablero.mover(movimiento, f2[1]);
+                    try{
+                        if (f2[1].getCasilla().getCamino()<0) {
+                            //implementacion cuando se trabaje con dados
+                        } else if (f2[1].getCasilla().getTipo().getNumero()==f2[1].getTipo().getId()
+                                ||f2[1].getCasilla().getCamino()>100) {
+                            tablero.mover(movimiento, f2[0]);
+                        } else {
+                            tablero.mover(tablero.PasaPorSeguroTorre(movimiento, f2[1]), f2[1]);
+                        }
+                    }catch (Exception e){
+                        System.out.println("No se puede mover la ficha tanto");
+                    }
                     System.out.println("Casilla donde termino la ficha: "+(f2[1].getCasilla().getCamino()+1));
                         break;
                     default:System.out.println("Eleccion inaceptable");
