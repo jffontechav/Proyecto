af5cbf5d4a865a42011f54dde8dba200a16a803e
Juan Felipe Fontecha <USUARIO@USUARIO-PC>
2016-11-25 12:48:41 -0500

    Version 2.3 completo la vesrion 1.0 del mapa de parques(2 carceles, 2 torres y un camino de 9 casillas de largo) completo movimiento normal y por las torres(para este mapa y sin pruebas exahustivas) *falta la opcion de comer, el uso de las carceles, los dados y la forma como hacer que las fichas entren en las torres en un primer lugar

---
+++ src/Excepciones/ExcepcionA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Excepciones;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class ExcepcionA extends Exception{
+
+    public ExcepcionA() {
+        super();
+    }
+
+    public ExcepcionA(String string) {
+        super(string);
+    }
+}
